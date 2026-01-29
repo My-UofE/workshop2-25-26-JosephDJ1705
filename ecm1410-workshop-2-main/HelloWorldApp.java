aaef3b67d5a6ae8146aa2109a48d811fd40dd87c
Josephhh <jj659@exeter.ac.uk>
2026-01-29 22:02:15 +0000

    changse

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -4,7 +4,7 @@
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! My name is " + args[0] + ".");
     }
 }
 
