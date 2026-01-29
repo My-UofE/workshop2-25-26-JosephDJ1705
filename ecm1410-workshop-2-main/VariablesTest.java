3bb1b16bc54d5c6dfbaa2010343ec7fff609d30d
Josephhh <jj659@exeter.ac.uk>
2026-01-29 23:06:51 +0000

    changes with different operations

--- ecm1410-workshop-2-main/VariablesTest.java
+++ ecm1410-workshop-2-main/VariablesTest.java
@@ -3,10 +3,18 @@ public class VariablesTest {
     public static void main(String[] args) {
         int x = '4';
         System.out.println("x: " + x);
-        byte s_per_min = 60;
+        long s_per_min = 60;
         System.out.println("s_per_min: " + s_per_min);
-        long s_per_day = (s_per_min * 60 * 24);
+        byte s_per_day = (byte) (s_per_min * 60 * 24);
         System.out.println("s_per_day: " + s_per_day);
+        float area = 20.3F;
+        System.out.println("area: " + area);
+        char a = '\u5468', b = '\\';
+        System.out.println("a and b: " + a + b);
+        int nHours = 022;
+        System.out.println("nHours: " + nHours);
+        double ratio = 7/5;
+        System.out.println("ratio: " + ratio)
     
     }
 }
