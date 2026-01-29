b9018a145ff3212f193e45188e5abe377cd509f6
Josephhh <jj659@exeter.ac.uk>
2026-01-29 23:25:27 +0000

    first task

---
+++ ecm1410-workshop-2-main/MyBirthday.java
@@ -0,0 +1,12 @@
+// MyBirthday application
+public class MyBirthday {
+    public static void main(String[] args) {
+        int currentDay = 2; 
+        int currentDate = 7; 
+        int myBirthdayDate = 23; 
+
+        // value below is a constant
+        final int DAYS_A_WEEK = 7; 
+
+        int daystobirthday = myBirthdayDate - currentDate
+        System.out.println("Days to birthday from current date: " + daystobirthday)
