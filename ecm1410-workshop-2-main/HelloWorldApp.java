4e1c268b9e74e1a2203fb0563c6be93ac569f3f4
Josephhh <jj659@exeter.ac.uk>
2026-01-29 22:08:33 +0000

    modification to the task

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -6,10 +6,10 @@ public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.print("Hello World! My name is ");
         System.out.print(args[0]);
-        System.out.println(".");
+        System.out.print(".");
         System.out.print("I am ");
         System.out.print(args[1]);
-        System.out.print("years old ");
+        System.out.print(" years old");
         System.out.println(".");
     }
 }
